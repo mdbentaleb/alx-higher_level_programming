f70cb5a663b91b908547011d19bc3c6ba87cce68
mdbentaleb <bent4leb@gmail.com>
2024-03-18 23:37:13 +0000

    -

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) < 1:
+        return None
+    else:
+        mx = my_list[0]
+        for j in range(len(my_list)):
+            if my_list[i] > mx:
+                mx = my_list[j]
+
+        return mx
