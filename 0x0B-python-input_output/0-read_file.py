27efd4c66d360611ceec33f3f4185045dbb3d509
mdbentaleb <bentalebm@outlook.com>
2024-04-17 00:30:50 +0100

    -

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" def read_file """
+
+
+def read_file(filename=""):
+    """reads a text file (UTF-8) and prints to stdout"""
+    with open(filename) as f:
+        for line in f:
+            print(line, end='')
