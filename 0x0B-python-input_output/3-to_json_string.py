bb4c442245f828d8ac2cbe308cbe970e8d48f4b6
mdbentaleb <bentalebm@outlook.com>
2024-04-17 00:39:52 +0100

    -

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" to json string """
+import json
+
+
+def to_json_string(my_obj):
+    """ to_json_string that return the JSON representation
+    of an object 'string' """
+    return (json.dumps(my_obj))
